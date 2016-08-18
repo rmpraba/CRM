f71f36405abc48237c194959988caed351c50845
rmpraba <rmpraba@gmail.com>
2016-08-18 12:30:07 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+ var express    = require("express");
+ var mysql      = require('mysql');
+ var email   = require("emailjs/email");
+ var connection = mysql.createConnection({
+   host     : 'localhost',
+   user     : 'root',
+   password : 'admin',
+   database : 'scorecarddb'
+ });
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(express.static('app'));
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.get('/', function (req, res) {
+   res.sendFile("app/index.html" );
+})
+
+
+var server = app.listen(6000, function () {
+var host = server.address().address
+var port = server.address().port
+console.log("Example app listening at http://%s:%s", host, port)
+});
