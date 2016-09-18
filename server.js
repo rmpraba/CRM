41fe8f820e3cb60ee243d36bde8712feef658ed1
Dhivakar Saravanan <dhivakarksaravanan@gmail.com>
2016-09-18 16:48:45 +0530

    crm start up

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
    host     : 'localhost',
    user     : 'root',
    password : 'admin',
-   database : 'scorecarddb'
+   database : 'mlzs-crm'
  });
 var bodyParser = require('body-parser');
 var app = express();
@@ -15,14 +15,70 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.get('/', function (req, res) {
    res.sendFile("app/index.html" );
-})
+});
 
+/*
+ app.post('/add-event' ,  urlencodedParser,function (req, res) {
+   var event = {
+     "title": req.query.title,
+     "description": req.query.description,
+     "start_date": req.startdate,
+     "end_date": req.query.enddate,
+     "event_type":req.query.event_type,
+     "occurrence_frequency": req.query.frequency,
+     "is_recurrence": req.query.is_recurrence,
+     "occurrences": req.query.occurrance,
+     "event_location": req.query.event_location,
+     "status": "1"
+   };
+   console.log('in server...');
+   console.log(event);
+   connection.query('insert into new_event set ?', [event],
+     function (err, rows) {
+       if (!err) {
+         console.log('inserted');
+         res.status(200).json({'returnval': 'success'});
+       } else {
+         console.log("error");
+         console.log(err);
+         res.status(200).json({'returnval': 'invalid'});
+       }
+     });
+ });
+*/
 
+ app.post('/add-event',  urlencodedParser,function (req, res){
+   var event = {
+     "title": req.query.title,
+     "description": req.query.description,
+     "start_date": req.startdate,
+     "end_date": req.query.enddate,
+     "event_type":req.query.event_type,
+     "occurrence_frequency": req.query.frequency,
+     "is_recurrence": req.query.is_recurrence,
+     "occurrences": req.query.occurrance,
+     "event_location": req.query.event_location,
+     "status": "1"
+   };
+   connection.query('insert into new_event set ?',[event],
+     function(err, rows){
+
+       if(!err)
+       {
+         res.status(200).json({'returnval': 'success'});
+       }
+       else
+       {
+         console.log(err);
+         res.status(200).json({'returnval': 'invalid'});
+       }
+     });
+ });
 function setvalue(){
   console.log("calling setvalue.....");
 }
 var server = app.listen(8086, function () {
-var host = server.address().address
-var port = server.address().port
-console.log("Example app listening at http://%s:%s", host, port)
+var host = server.address().address;
+var port = server.address().port;
+console.log("Example app listening at http://%s:%s", host, port);
 });
