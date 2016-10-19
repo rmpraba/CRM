859da85a0393e43cb96824064cc55f6b3e258ffd
Mohamed Abbas <softabbas@gmail.com>
2016-10-19 10:24:02 +0530

    simpleform

--- server.js
+++ server.js
@@ -191,7 +191,12 @@ app.post('/createinstallment-service',  urlencodedParser,function (req, res){
 /*This function is used to submit the simple enquiry details of the student for the first time*/
 app.post('/submitenquiry',  urlencodedParser,function (req, res)
 {
-  var collection={"enquiry_no":req.query.id,"school_id":req.query.schol,"academic_year":req.query.acadeyr,"class":req.query.grade,"father_mob":req.query.contact,"gender":req.query.gender,"first_name":req.query.firstname,"middle_name":req.query.middlename,"last_name":req.query.lastname,"dob":req.query.dobs,"father_name":req.query.father,"locality":req.query.location,"mother_name":req.query.mother,"father_email":req.query.email,"created_by":req.query.createdby,"created_on":req.query.createdate,"enquiry_name":req.query.givenname,"status":"Enquired"};
+  var collection={"enquiry_no":req.query.id,"school_id":req.query.schol,"academic_year":req.query.acadeyr,
+  "class":req.query.grade,"father_mob":req.query.contact,"gender":req.query.gender,
+  "first_name":req.query.firstname,"middle_name":req.query.middlename,"last_name":req.query.lastname,
+  "dob":req.query.dobs,"father_name":req.query.father,"locality":req.query.location,"mother_name":req.query.mother,
+  "father_email":req.query.email,"created_by":req.query.createdby,"created_on":req.query.createdate,
+  "enquiry_name":req.query.givenname,"gemail":req.query.gemail,"gmob":req.query.gmob,"Guardianname":req.query.Guardianname,"status":"Enquired"};
        connection.query('insert into student_enquiry_details set ? ',[collection],
         function(err, rows)
         {
